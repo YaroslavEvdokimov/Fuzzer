06d88906e14837d0908fb5e93c6a58613ac24459
YaroslavEvdokimov <76532905+YaroslavEvdokimov@users.noreply.github.com>
2021-10-04 22:45:05 +0300

    Add files via upload

--- converter/ConverterStart.h
+++ converter/ConverterStart.h
@@ -8,7 +8,7 @@ class Converter {
 private:
     std::string PathEntrance;
     std::string PathOutput;
-    std::vector<char> BinaryText;
+    std::vector<std::uint8_t> BinaryText;
 public:
     Converter(std::string path_entrance, std::string path_output);
 
